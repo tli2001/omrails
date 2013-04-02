2cbf9a31e8600498b59756fc5c27eec360ecc52c
tli2001a <tli2001a@yahoo.com>
2013-04-02 00:23:26 -0700

    Update pin.rb

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -11,7 +11,7 @@ class Pin < ActiveRecord::Base
   has_attached_file :image, styles: {medium: "320x240>"}
   
   def image_remote_url=(url_value)
-  	self.image = URI.parse(url_value) unless url_value.blank?
-  	super
+    self.image = URI.parse(url_value) unless url_value.blank?
+    super
   end
 end
